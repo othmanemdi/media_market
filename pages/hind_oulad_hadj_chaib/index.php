53ac9a5bb402c4affdc3d2bc61679fb89de86d40
Hind Oulad Hadj Chaib <hindoulad57@gmail.com>
2022-06-01 20:31:49 +0100

    third commit

--- pages/hind_oulad_hadj_chaib/index.php
+++ pages/hind_oulad_hadj_chaib/index.php
@@ -1,15 +1,63 @@
 <?php
 
-ob_start();
-// php
-$title = "Shop";
+// var_dump(dirname(__DIR__) . DIRECTORY_SEPARATOR);
+// die();
+if (isset($_GET['page']) && preg_match("/^[a-zA-Z0-9_-]*$/", $_GET['page'])) {
+    $page = htmlspecialchars(trim($_GET['page']));
+} else {
+    $page = "home";
+}
 
-$content_php = ob_get_clean();
+require_once "database/db.php";
 
 
-ob_start(); ?>
+$pages =  scandir('pages/');
+require_once "helpers/functions.php";
 
-<h1>hello hind</h1>
+$page_file = $page . ".php";
+
+if (in_array($page_file, $pages)) {
+    require_once 'pages/' . $page_file;
+} else {
+    require_once 'pages/404.php';
+}
+
+echo $content_php ?? "";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?= $content_seo ?? ""; ?>
+
+    <?php require_once "pages/body/head.php" ?>
+
+    <?= $content_css ?? ""; ?>
+
+    <?php require_once "pages/body/script.php" ?>
+</head>
+
+<body>
+
+    <?php require_once "pages/body/info.php" ?>
+
+    <div class="container mt-4">
+        <?php require_once "pages/body/header.php" ?>
+        <?php require_once "pages/body/nav.php" ?>
+
+        <?= $content_html ?? "" ?>
+    </div>
+
+    <?php require_once "pages/body/back-to-top.php" ?>
 
 
-<?php $content_html = ob_get_clean(); ?>
+    <footer>
+        <?php require_once "pages/body/footer.php" ?>
+    </footer>
+
+    <?= $content_js ?? ""; ?>
+
+</body>
+
+</html>
