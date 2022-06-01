c827a5cdc83573db6ee3e806d90266ea003a9063
Hind Oulad Hadj Chaib <hindoulad57@gmail.com>
2022-06-01 19:44:15 +0100

    My second commit

--- pages/hind_oulad_hadj_chaib/index.php
+++ pages/hind_oulad_hadj_chaib/index.php
@@ -1,1 +1,15 @@
-<h1>HELLO HIND</h1>
+<?php
+
+ob_start();
+// php
+$title = "Shop";
+
+$content_php = ob_get_clean();
+
+
+ob_start(); ?>
+
+<h1>hello hind</h1>
+
+
+<?php $content_html = ob_get_clean(); ?>
