2ab75f5fa261721cdf3a777627d15837b034a15a
Mariam Bouchuariti <bouchuaritimari@gmail.com>
2022-06-01 20:51:41 +0200

    my nav,header,info,footer

---
+++ pages/body/header_mariam.php
@@ -0,0 +1,58 @@
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-auto me-auto mb-3">
+            <img src="images/logo/media_market_logo.png" alt="" height="70" class="mt-0">
+        </div>
+
+        <div class="col-md-5 me-auto mb-3">
+
+            <div class="input-group input-group-lg">
+                <input type="text" class="form-control border-dark border-2 rounded-end rounded-pill " placeholder="Saisissez votre recherche...">
+                <button class="btn bg-dark text-white rounded-start rounded-pill px-4">
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+        </div>
+
+        <div class="col-auto mb-4">
+            <div class="ms-4 mt-3">
+                <a href="cart" class="position-relative text-dark">
+                    <i class="fa-solid fa-user"></i>
+                    <span class="position-absolute top-75 start- translate-middle badge bg-info rounded-pill">
+                        0
+                    </span>
+                </a>
+            </div>
+        </div>
+
+
+        <div class="col-auto mb-4">
+            <div class="ms-4 mt-3">
+                <a href="cart" class="position-relative text-dark">
+                    <i class="fa-solid fa-heart"></i>
+                    <span class="position-absolute top-75 start- translate-middle badge bg-info rounded-pill">
+                        0
+                    </span>
+                </a>
+            </div>
+        </div>
+
+
+        <div class="col-auto mb-3">
+            <div class="ms-4 mt-3">
+                <a href="cart" class="position-relative text-dark">
+                    <i class="fas fa-shopping-cart fs-5"></i>
+                    <span class="position-absolute top-75 start-150 translate-middle badge bg-info rounded-pill">
+                        3
+                    </span>
+
+                </a>
+
+
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
---
+++ pages/body/info_mariam.php
@@ -0,0 +1,44 @@
+<div class=" bg-dark p-1">
+    <div class="container">
+        <div class="text-center text-white fw-bold">
+            <span class="text-info">⭐⭐</span>
+             RANGEZ BIEN, À PRIX MALIN ! REMISE JUSQU'À 50% 
+             <span class="text-info">⭐⭐</span>
+        </div>
+    </div>
+</div>
+<div class="bg-light p-1">
+    <div class="container">
+        <div class="d-flex">
+            <ul class="me-auto list-inline">
+                <li class="list-inline-item text-kitea-hover">
+                    <i class="fas fa-question-circle fa-fw text-kitea"></i>
+                    Besoin d'aide ?
+                </li>
+                <li class="list-inline-item text-kitea-hover">
+                    <i class="fas fa-envelope fa-fw text-kitea"></i>
+                    service.client@media_market.ma
+                </li>
+                <li class="list-inline-item text-kitea-hover">
+                    <i class="fas fa-phone fa-fw text-kitea"></i>
+                    0802 00 8002
+                </li>
+            </ul>
+
+            <ul class="list-inline">
+                <li class="list-inline-item text-kitea-hover">
+                    <i class="fas fa-map-marker-alt fa-fw text-kitea"></i>
+                    Choisissez votre ville
+                </li>
+                <li class="list-inline-item text-kitea-hover">
+                    <i class="fas fa-store-alt fa-fw text-kitea"></i>
+                    Nos magasins
+                </li>
+            </ul>
+
+        </div>
+
+
+
+    </div>
+</div>
