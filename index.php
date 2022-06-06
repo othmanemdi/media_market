5ec507b9852437a0bb290956313ce147493695c8
Othmane Mezgueldi <othmane.mezgueldi@gmail.com>
2022-06-06 20:30:55 +0000

    Add session message flash

--- index.php
+++ index.php
@@ -41,9 +41,18 @@ echo $content_php ?? "";
     <?php require_once "pages/body/info.php" ?>
     <?php require_once "pages/body/header.php" ?>
     <?php require_once "pages/body/nav.php" ?>
-    
+
 
     <div class="container mt-5">
+
+        <?php if (isset($_SESSION['flash'])) : ?>
+            <?php foreach ($_SESSION['flash'] as $type => $message) : ?>
+                <div class="alert alert-<?= $type; ?>">
+                    <?= $message; ?>
+                </div>
+            <?php endforeach; ?>
+            <?php unset($_SESSION['flash']); ?>
+        <?php endif; ?>
 
         <?= $content_html ?? "" ?>
     </div>
