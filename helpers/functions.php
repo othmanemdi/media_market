ef0febfe7e1a92e15e9a651813103f796eb25ae3
Othmane Mezgueldi <othmane.mezgueldi@gmail.com>
2022-06-11 19:22:02 +0000

    Marque CRUDS

--- helpers/functions.php
+++ helpers/functions.php
@@ -2,3 +2,11 @@
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
+
+function dd(array $array)
+{
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+    die();
+}
--- pages/404.php
+++ pages/404.php
@@ -9,7 +9,7 @@ $content_php = ob_get_clean();
 
 ob_start(); ?>
 
-<h1>404 page</h1>
+<h3>404 page</h3>
 
 
 <?php $content_html = ob_get_clean(); ?>
